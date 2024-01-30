4d60c21fd350db9eab9ea553233ee1b9b87fae52
Micha Buchholz <micha@haus23.net>
2024-01-30 12:10:09 +0100

    chore: Add dedicated ports.

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -2,5 +2,8 @@ import { unstable_vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    port: 5001,
+  },
   plugins: [remix()],
 });
