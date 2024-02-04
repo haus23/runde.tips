24efaa76309c5272e18030b700471d90654944fa
Micha Buchholz <micha@haus23.net>
2024-02-04 18:40:34 +0100

    fix: Use default cursor on buttons.

--- packages/ui/src/(ui)/button/button.tsx
+++ packages/ui/src/(ui)/button/button.tsx
@@ -8,7 +8,7 @@ import { focusRing } from '../base-styles';
 
 const button = tv({
   extend: focusRing,
-  base: 'flex px-4 py-2 rounded-md text-center',
+  base: 'flex px-4 py-2 rounded-md text-center cursor-default',
   variants: {
     variant: {
       neutral: 'bg-cn hover:bg-cn-hover pressed:bg-cn-active',
