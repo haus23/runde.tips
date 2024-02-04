932332a14459d2e7b4dc86a47c26ef4dea625833
Micha Buchholz <micha@haus23.net>
2024-02-04 14:22:46 +0100

    fix: Typo.

--- apps/www/tailwind.config.ts
+++ apps/www/tailwind.config.ts
@@ -2,6 +2,6 @@ import sharedConfig from '@tipprunde/tailwind-config';
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./app/**/*.{ts,tsx}', '../../packages/ui/**/*.{tx,tsx}'],
+  content: ['./app/**/*.{ts,tsx}', '../../packages/ui/**/*.{ts,tsx}'],
   presets: [sharedConfig],
 } satisfies Config;
