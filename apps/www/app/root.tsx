0561bcb650c5ece0e079597d939d7ef1dce3796c
Micha Buchholz <micha@haus23.net>
2024-02-06 19:30:27 +0100

    fix: Hydration error since we have a default scheme. And better dark/light mode switching via color-scheme.

--- apps/www/app/root.tsx
+++ apps/www/app/root.tsx
@@ -40,7 +40,7 @@ function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="color-scheme" content="dark light" />
+        <meta name="color-scheme" content={theme.colorScheme} />
         <MediaQueryFallback />
         <Meta />
         <Links />
--- packages/utils/src/theme/media-query-fallback.tsx
+++ packages/utils/src/theme/media-query-fallback.tsx
@@ -5,9 +5,20 @@ const clientThemeCode = `
   const theme = window.matchMedia('(prefers-color-scheme: light)').matches
     ? 'light'
     : 'dark';
+  const otherTheme = theme === 'light' ? 'dark' : 'light';
 
   const cl = document.documentElement.classList;
   cl.toggle(theme, true);
+  cl.toggle(otherTheme, false);
+
+  const meta = document.querySelector('meta[name=color-scheme]');
+  if (meta) {
+    if (theme === 'dark') {
+      meta.content = 'dark';
+    } else if (theme === 'light') {
+      meta.content = 'light';
+    }
+  }
 })();
 `;
 
