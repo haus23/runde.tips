31227fae78725bfde1981686d17b3390d918b945
Micha Buchholz <micha@haus23.net>
2024-02-09 10:33:28 +0100

    refactor: Clean up and add manager link.

--- apps/www/app/routes/_foh+/_layout/user-menu.tsx
+++ apps/www/app/routes/_foh+/_layout/user-menu.tsx
@@ -1,43 +1,8 @@
-import {
-  Button,
-  Icon,
-  type IconName,
-  type Key,
-  Menu,
-  MenuItem,
-  MenuItems,
-} from '@tipprunde/ui';
-import type { ColorScheme } from '#app/types';
-import { includes } from '#app/utils/misc';
-import { useTheme } from '#app/utils/theme';
-
-const colorSchemes: {
-  name: ColorScheme | 'system';
-  label: string;
-  icon: IconName;
-}[] = [
-  { name: 'light', label: 'Hell', icon: 'lucide/sun' },
-  { name: 'dark', label: 'Dunkel', icon: 'lucide/moon' },
-  { name: 'system', label: 'System', icon: 'lucide/laptop' },
-];
+import { Button, Icon, Menu, MenuItem, MenuItems } from '@tipprunde/ui';
+import { useUser } from '#app/utils/user';
 
 export function UserMenu() {
-  const { theme, mode, setTheme } = useTheme();
-
-  const selectedColorScheme = new Set([
-    mode === 'session' ? theme.colorScheme : 'system',
-  ]);
-
-  function handleAction(key: Key) {
-    if (
-      includes(
-        colorSchemes.map((cs) => cs.name),
-        key,
-      )
-    ) {
-      !selectedColorScheme.has(key) && setTheme({ ...theme, colorScheme: key });
-    }
-  }
+  const user = useUser();
 
   return (
     <Menu>
@@ -45,8 +10,13 @@ export function UserMenu() {
         <Icon name="lucide/user" />
       </Button>
       <MenuItems placement="bottom" showArrow>
+        {user.role.includes('ADMIN') && (
+          <MenuItem className="cursor-pointer" href="/manager">
+            <Icon name="lucide/settings">Manager</Icon>
+          </MenuItem>
+        )}
         <MenuItem className="cursor-pointer" href="/logout">
-          Log Out
+          <Icon name="lucide/log-out">Log Out</Icon>
         </MenuItem>
       </MenuItems>
     </Menu>
