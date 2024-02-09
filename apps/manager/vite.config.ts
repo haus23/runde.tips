cac7a6cddaafa9af66b4e95e34a7171f500c87a3
Micha Buchholz <micha@haus23.net>
2024-02-09 19:32:19 +0100

    fix: Remove the manager app. See issue #3

--- apps/manager/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { unstable_vitePlugin as remix } from '@remix-run/dev';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  server: {
-    port: 5002,
-  },
-  plugins: [remix()],
-});
