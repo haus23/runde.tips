a82fdf7c7a62aa2d107e3eebcfadc50271767cb0
Micha Buchholz <micha@haus23.net>
2024-02-22 08:47:36 +0100

    fix: Update notice about the sync backend reasons.

--- app/routes/manager+/sync/_sync.tsx
+++ app/routes/manager+/sync/_sync.tsx
@@ -32,8 +32,7 @@ export default function SyncRoute() {
         <p className="text-app-notice">
           Hiermit werden die Cache-Daten des Backends gelöscht. Damit kann die
           zur Zeit noch laufende Tipprunden-Anwendung wieder aktuelle Daten
-          anzeigen und wir bei den Synchronisierungen unten ebenfalls auf
-          aktuellste Daten zurückgreifen.
+          anzeigen.
         </p>
         <p className="text-app-notice">
           Zur Zeit sind die zu löschenden Routen noch hart codiert. Während
