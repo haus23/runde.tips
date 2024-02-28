7dea67e955916d52be5f3030f87e8165054968ee
Micha Buchholz <micha@haus23.net>
2024-02-28 14:11:09 +0100

    refactor: Align build path with vite. Looks better even w/o vite.

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,8 @@ import { flatRoutes } from 'remix-flat-routes';
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  serverBuildPath: 'build/server/index.js',
+  assetsBuildDirectory: 'build/client',
   ignoredRouteFiles: ['**/*'],
   routes: async (defineRoutes) =>
     flatRoutes('routes', defineRoutes, { ignoredRouteFiles: ['**/*.md'] }),
