d90e390de977528fc161fc3f151fd5d23501e222
Micha Buchholz <micha@haus23.net>
2024-03-01 20:41:46 +0100

    fix: Logging out manually. So precise control over the redirect path. Resolves #44

--- app/routes/_foh+/_auth+/logout.tsx
+++ app/routes/_foh+/_auth+/logout.tsx
@@ -1,12 +1,26 @@
 import { type ActionFunctionArgs, redirect } from '@remix-run/node';
-import { authenticator } from '#utils/auth/auth.server';
+import { redirectBack } from 'remix-utils/redirect-back';
+import { authSessionStorage } from '#utils/auth/auth.server';
 
 export function loader() {
   return redirect('/');
 }
 
 export async function action({ request }: ActionFunctionArgs) {
-  return await authenticator.logout(request, {
-    redirectTo: '/',
+  const authSession = await authSessionStorage.getSession(
+    request.headers.get('Cookie'),
+  );
+  const referer = request.headers.get('Referer') || '/';
+  const isManagerRoute = new URL(referer).pathname.startsWith('/manager');
+
+  if (isManagerRoute) {
+    request.headers.delete('Referer');
+  }
+
+  throw redirectBack(request, {
+    fallback: '/',
+    headers: {
+      'Set-Cookie': await authSessionStorage.destroySession(authSession),
+    },
   });
 }
