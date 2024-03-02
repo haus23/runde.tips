29dbb21a44894bf873aa641b98404245bbf00e75
Micha Buchholz <micha@haus23.net>
2024-03-02 17:21:05 +0100

    feat: Load relevant data.

--- app/routes/_foh+/($championship)+/index/_ranking.tsx
+++ app/routes/_foh+/($championship)+/index/_ranking.tsx
@@ -1,18 +1,47 @@
 import { type LoaderFunctionArgs, json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
+import { db } from '#utils/db.server';
 import { requireChampionship } from '#utils/foh/championships.server';
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const championship = await requireChampionship(params);
-  return json({ championship });
+  const ranks = await db.player.findMany({
+    where: { championshipId: championship.id },
+    orderBy: { rank: 'asc' },
+    include: { user: true },
+  });
+
+  return json({ championship, ranks });
 }
 
 export default function RankingRoute() {
-  const { championship } = useLoaderData<typeof loader>();
+  const { championship, ranks } = useLoaderData<typeof loader>();
 
   return (
     <div>
-      <h2 className="text-3xl font-medium">{championship.name}</h2>
+      <h2 className="text-3xl font-medium">
+        {championship.name} - Abschlusstabelle
+      </h2>
+      <table>
+        <thead>
+          <tr>
+            <td>Platz</td>
+            <td>Name</td>
+            <td>Zusatzpunkte</td>
+            <td>Gesamtpunkte</td>
+          </tr>
+        </thead>
+        <tbody>
+          {ranks.map((r) => (
+            <tr key={r.id}>
+              <td>{r.rank}</td>
+              <td>{r.user.name}</td>
+              <td>{r.extraPoints}</td>
+              <td>{r.totalPoints}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
