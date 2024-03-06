7c58fd99cae5f3ac9544601860751b11f2b5fbe2
Micha Buchholz <micha@haus23.net>
2024-03-06 19:27:59 +0100

    feat: Add mobile navmenu in foh.

--- app/routes/_foh+/_layout/app-header.tsx
+++ app/routes/_foh+/_layout/app-header.tsx
@@ -3,20 +3,35 @@ import {
   NavbarBrand,
   NavbarContent,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
 } from '@nextui-org/react';
+import { useState } from 'react';
 import { Logo, ThemeMenu } from '#components';
 
 export function AppHeader() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <Navbar>
-      <NavbarBrand>
-        <Logo />
-      </NavbarBrand>
-      <NavbarContent justify="end">
+    <Navbar isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle className="sm:hidden" />
+        <NavbarBrand className="hidden sm:flex">
+          <Logo />
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent justify="end" className="hidden sm:flex">
         <NavbarItem>
           <ThemeMenu />
         </NavbarItem>
       </NavbarContent>
+      <NavbarMenu>
+        <NavbarMenuItem className="flex items-center justify-between">
+          <span>Hell-/Dunkelmodus</span>
+          <ThemeMenu />
+        </NavbarMenuItem>
+      </NavbarMenu>
     </Navbar>
   );
 }
