27bb3cfe8e2d655302cded7304dc596addaf5a9c
Micha Buchholz <micha@haus23.net>
2024-03-12 20:56:46 +0100

    fix: Add simple but working popover. Resolves #76

---
+++ app/components/ui/popover/popover.tsx
@@ -0,0 +1,27 @@
+import {
+  Popover,
+  type PopoverProps,
+  composeRenderProps,
+} from 'react-aria-components';
+import { tv } from 'tailwind-variants';
+
+const styles = tv({
+  base: [
+    'bg-popover border border-default rounded-xl shadow-lg',
+    'forced-colors:bg-[Canvas]',
+  ],
+});
+
+function _Popover({ className, ...props }: PopoverProps) {
+  return (
+    <Popover
+      offset={8}
+      className={composeRenderProps(className, (className, renderProps) =>
+        styles({ ...renderProps, className }),
+      )}
+      {...props}
+    />
+  );
+}
+
+export { _Popover as Popover };
