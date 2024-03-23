08017c37e20399737dc397fccce5ec47570012e1
Micha Buchholz <micha@haus23.net>
2024-03-23 19:50:42 +0100

    feat: Add user-menu and logout action.

--- app/components/user-menu.tsx
+++ app/components/user-menu.tsx
@@ -1,3 +1,34 @@
+import { useSubmit } from '@remix-run/react';
+import { type Key, Menu, MenuItem, MenuTrigger } from 'react-aria-components';
+import { useUser } from '#utils/auth/user';
+import { Button, Icon } from './ui';
+import { Popover } from './ui/popover/popover';
+
 export function UserMenu() {
-  return <></>;
+  const submit = useSubmit();
+  const user = useUser();
+
+  function handleAction(key: Key) {
+    if (key === 'logout') {
+      submit(null, { method: 'post', action: '/logout' });
+    }
+  }
+
+  return (
+    <MenuTrigger>
+      <Button variant="toolbar" className="aria-expanded:opacity-70">
+        <Icon name="lucide/user" />
+      </Button>
+      <Popover placement="bottom">
+        <Menu className="p-1.5 w-[180px] outline-none" onAction={handleAction}>
+          <MenuItem
+            id="logout"
+            className="flex items-center text-sm select-none py-2 pl-3 pr-1 rounded-lg outline-none transition-colors data-[focused]:bg-content-hover"
+          >
+            <Icon name="lucide/log-out">Log Out</Icon>
+          </MenuItem>
+        </Menu>
+      </Popover>
+    </MenuTrigger>
+  );
 }
