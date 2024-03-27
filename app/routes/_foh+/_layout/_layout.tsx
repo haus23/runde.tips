36b829f0bb2537cc9b281279311b6a6455d081da
Micha Buchholz <micha@haus23.net>
2024-03-27 21:20:32 +0100

    feat: Finally add a championship provider. Wasn't that hard ;-)

--- app/routes/_foh+/_layout/_layout.tsx
+++ app/routes/_foh+/_layout/_layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet, json } from '@remix-run/react';
+import { ChampionshipProvider } from '#utils/foh/championship.context';
 import { getPublishedChampionships } from '#utils/foh/championships.server';
 import { Header } from './header';
 
@@ -9,11 +10,11 @@ export async function loader() {
 
 export default function FohLayout() {
   return (
-    <>
+    <ChampionshipProvider>
       <Header />
       <main className="mx-auto max-w-4xl pb-10 sm:px-6 lg:px-8 mt-2">
         <Outlet />
       </main>
-    </>
+    </ChampionshipProvider>
   );
 }
