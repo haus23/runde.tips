709f301c632fdba8b9113bfd8fea407f5cec4c20
Micha Buchholz <micha@haus23.net>
2024-03-28 23:11:18 +0100

    feat: Sync initially current championship with preps for subsequent syncs. Resolves #100

--- app/utils/sync/championship.ts
+++ app/utils/sync/championship.ts
@@ -33,6 +33,7 @@ export async function syncChampionship(
     if (legacyChampionship.completed) {
       return await syncBySimpleInsert(legacyChampionship, taskId);
     }
+    return await syncByPrepareTrackingData(legacyChampionship, taskId);
   }
 
   throw new Error(
@@ -177,4 +178,157 @@ async function syncBySimpleInsert(
   }
 
   return championship;
+}
+
+async function syncByPrepareTrackingData(
+  legacyCampionship: Awaited<ReturnType<typeof getFirestoreChampionshipById>>,
+  taskId: string,
+) {
+  const legacyData = await getLegacyStandings(legacyCampionship.id);
+  const masterData = await getMasterData();
+
+  const ruleset = masterData.rulesets.find(
+    (rs) => rs.slug === legacyCampionship.rulesId,
+  );
+  if (!ruleset) {
+    throw new Error(`Unknown ruleset ${legacyCampionship.rulesId}`);
+  }
+
+  const championship = await db.championship.create({
+    data: {
+      slug: legacyCampionship.id,
+      name: legacyCampionship.name,
+      nr: legacyCampionship.nr,
+      published: legacyCampionship.published,
+      completed: legacyCampionship.completed,
+      extraPointsPublished: legacyCampionship.extraPointsPublished,
+      rulesetId: ruleset.id,
+    },
+  });
+
+  // Extract rounds
+  for (const r of legacyData.legacyRounds) {
+    const round = await db.round.create({
+      data: {
+        nr: r.nr,
+        isDoubleRound: r.isDoubleRound,
+        championshipId: championship.id,
+        published: true,
+        completed: true,
+        tipsPublished: true,
+      },
+    });
+    r.entityId = round.id;
+    await db.firestoreMapping.create({
+      data: { firebaseId: r.id, sqliteId: round.id },
+    });
+  }
+
+  // Extract matches
+  for (const m of legacyData.legacyMatches) {
+    const round = legacyData.legacyRounds.find((r) => r.id === m.roundId);
+    invariant(typeof round !== 'undefined');
+
+    const league = masterData.leagues.find((l) => l.slug === m.leagueId);
+    const hometeam = masterData.teams.find((t) => t.slug === m.hometeamId);
+    const awayteam = masterData.teams.find((t) => t.slug === m.awayteamId);
+
+    const optionalProps = {
+      date: m.date || undefined,
+      leagueId: league?.id,
+      hometeamId: hometeam?.id,
+      awayteamId: awayteam?.id,
+    };
+
+    const match = await db.match.create({
+      data: {
+        nr: m.nr,
+        result: m.result,
+        points: m.points,
+        championshipId: championship.id,
+        roundId: round.entityId,
+        ...optionalProps,
+      },
+    });
+    m.entityId = match.id;
+    await db.firestoreMapping.create({
+      data: { firebaseId: m.id, sqliteId: match.id },
+    });
+  }
+
+  // Extract players
+  for (const p of legacyData.legacyPlayers) {
+    const user = masterData.users.find((u) => u.slug === p.playerId);
+    invariant(typeof user !== 'undefined');
+
+    const player = await db.player.create({
+      data: {
+        points: p.points,
+        extraPoints: p.extraPoints,
+        totalPoints: p.totalPoints,
+        rank: p.rank,
+        userId: user.id,
+        championshipId: championship.id,
+      },
+    });
+    p.entityId = player.id;
+    await db.firestoreMapping.create({
+      data: { firebaseId: p.id, sqliteId: player.id },
+    });
+  }
+
+  taskProgressEventBus.emit({
+    taskId,
+    message: '... und jetzt noch die Tipps.',
+  });
+
+  // Extract tips and keep order of tips
+  for (const p of legacyData.legacyPlayers) {
+    const legacyPlayerTips = legacyData.legacyTips.filter(
+      (lt) => lt.playerId === p.id,
+    );
+
+    for (const m of legacyData.legacyMatches) {
+      const legacyTip = legacyPlayerTips.find((lt) => lt.matchId === m.id);
+      if (!legacyTip) continue;
+
+      const tip = await db.tip.create({
+        data: {
+          championshipId: championship.id,
+          playerId: p.entityId,
+          matchId: m.entityId,
+          tip: legacyTip.tip,
+          joker: legacyTip.joker || undefined,
+          extraJoker: legacyTip.extraJoker || undefined,
+          lonelyHit: legacyTip.lonelyHit || undefined,
+          points: legacyTip.points,
+        },
+      });
+      await db.firestoreMapping.create({
+        data: { firebaseId: legacyTip.id, sqliteId: tip.id },
+      });
+    }
+  }
+
+  return championship;
+}
+
+async function syncWithPreparedTrackingData(
+  legacyCampionship: Awaited<ReturnType<typeof getFirestoreChampionshipById>>,
+  taskId: string,
+) {
+  // 1. Turnier muss aktualisiert werden weil mglweise Zusatztipps veröffentlicht wurden
+  //    -> Neues Turnier kann über den Slug gefunden werden
+  // 2. Runden müssten zur Zeit nicht aktualisiert werden
+  //    -> Runde könnte gefunden werden über Turnier-ID und Runden-Nr
+  //    -> Ich werde aber in der Mapping-Tabelle einen Eintrag machen
+  // 3. Spiele müssen aktualisiert werden (Datum, Ergebnis, ...)
+  //    -> Spiel könnte gefunden werden über Turnier-ID und Spiel-Nr
+  //    -> Ich werde aber in der Mapping-Tabelle einen Eintrag machen
+  // 4. Spieler müssen aktualisiert werden (Punkte, ...)
+  //    -> Spieler könnte gefunden werden über Turnier-ID und Spieler-Slugr
+  //    -> Ich werde aber in der Mapping-Tabelle einen Eintrag machen
+  // 5. Tipps müssen aktualisiert werden
+  //    -> Hier muss die Mapping-Tabelle benutzt werden
+  //    -> Achtung: nachträglich hinzugefügte Tipps müssen erfasst werden (upsert?)
 }
