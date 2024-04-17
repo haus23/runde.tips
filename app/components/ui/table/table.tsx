e3196f37617902df6dcb9261a731d74f25cbd1c6
Micha Buchholz <micha@haus23.net>
2024-04-17 21:47:11 +0200

    fix: Implement table wrapper. Simplifies usage in ranking. Fixes #115

--- app/components/ui/table/table.tsx
+++ app/components/ui/table/table.tsx
@@ -5,20 +5,37 @@ import {
   type ColumnProps,
   Row,
   type RowProps,
+  Table,
+  TableBody,
+  type TableBodyProps,
+  TableHeader,
+  type TableHeaderProps,
+  type TableProps,
+  composeRenderProps,
 } from 'react-aria-components';
-import { twMerge } from 'tailwind-merge';
 import { tv } from 'tailwind-variants';
 import { focusRingStyles } from '../base-styles';
-import { composeTailwindRenderProps } from '../utils';
+
+const tableStyles = tv({
+  base: 'font-semibold text-sm',
+});
+
+const tableHeaderStyles = tv({
+  base: 'bg-accent text-xs uppercase',
+});
+
+const tableBodyStyles = tv({
+  base: 'divide-y divide-default',
+});
 
 const columnStyles = tv({
   extend: focusRingStyles,
-  base: '',
+  base: 'p-2 md:px-6',
 });
 
 const cellStyles = tv({
   extend: focusRingStyles,
-  base: '',
+  base: 'p-2 md:px-6',
 });
 
 const rowStyles = tv({
@@ -26,52 +43,78 @@ const rowStyles = tv({
   base: '',
 });
 
-interface _ColumnProps extends ColumnProps {
-  className?: string;
+function _Table({ className, ...props }: TableProps) {
+  return (
+    <Table
+      {...props}
+      className={composeRenderProps(className, (className, renderProps) =>
+        tableStyles({ ...renderProps, className }),
+      )}
+    />
+  );
 }
 
-function _Column({ className, ...props }: _ColumnProps) {
+function _TableHeader<T extends object>({
+  className,
+  ...props
+}: TableHeaderProps<T>) {
+  return (
+    <TableHeader {...props} className={tableHeaderStyles({ className })} />
+  );
+}
+
+function _Column({ className, ...props }: ColumnProps) {
   return (
     <Column
       {...props}
-      className={composeTailwindRenderProps(
-        (renderProps) => columnStyles({ ...renderProps }),
-        twMerge(className),
+      className={composeRenderProps(className, (className, renderProps) =>
+        columnStyles({ ...renderProps, className }),
       )}
     />
   );
 }
 
-interface _CellProps extends CellProps {
-  className?: string;
+function _TableBody<T extends object>({
+  className,
+  ...props
+}: TableBodyProps<T>) {
+  return (
+    <TableBody
+      {...props}
+      className={composeRenderProps(className, (className, renderProps) =>
+        tableBodyStyles({ ...renderProps, className }),
+      )}
+    />
+  );
 }
 
-function _Cell({ className, ...props }: _CellProps) {
+function _Cell({ className, ...props }: CellProps) {
   return (
     <Cell
       {...props}
-      className={composeTailwindRenderProps(
-        (renderProps) => cellStyles({ ...renderProps }),
-        twMerge(className),
+      className={composeRenderProps(className, (className, renderProps) =>
+        cellStyles({ ...renderProps, className }),
       )}
     />
   );
 }
 
-interface _RowProps<T> extends RowProps<T> {
-  className?: string;
-}
-
-function _Row<T extends object>({ className, ...props }: _RowProps<T>) {
+function _Row<T extends object>({ className, ...props }: RowProps<T>) {
   return (
     <Row
       {...props}
-      className={composeTailwindRenderProps(
-        (renderProps) => rowStyles({ ...renderProps }),
-        twMerge(className),
+      className={composeRenderProps(className, (className, renderProps) =>
+        rowStyles({ ...renderProps, className }),
       )}
     />
   );
 }
 
-export { _Column as Column, _Cell as Cell, _Row as Row };
+export {
+  _Table as Table,
+  _TableHeader as TableHeader,
+  _TableBody as TableBody,
+  _Column as Column,
+  _Cell as Cell,
+  _Row as Row,
+};
--- app/routes/_foh+/($championship)+/index/_ranking.tsx
+++ app/routes/_foh+/($championship)+/index/_ranking.tsx
@@ -1,7 +1,6 @@
 import { type LoaderFunctionArgs, json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import clsx from 'clsx';
-import { Table, TableBody, TableHeader } from 'react-aria-components';
 import { Fragment } from 'react/jsx-runtime';
 import {
   Button,
@@ -11,6 +10,9 @@ import {
   HoverBoxContent,
   Icon,
   Row,
+  Table,
+  TableBody,
+  TableHeader,
 } from '#components/ui';
 import { db } from '#utils/db.server';
 import { requireChampionship } from '#utils/foh/championships.server';
@@ -42,32 +44,32 @@ export default function RankingRoute() {
           {championship.completed ? 'Abschlusstabelle' : 'Aktuelle Tabelle'}
         </h2>
       </div>
-      <Table className="font-semibold text-sm" aria-labelledby="tableLabel">
-        <TableHeader className="bg-accent text-xs uppercase">
-          <Column className="px-2 py-2 text-right md:px-6">Platz</Column>
-          <Column className="px-2 text-left md:px-6" isRowHeader>
+      <Table aria-labelledby="tableLabel">
+        <TableHeader>
+          <Column className="text-right">Platz</Column>
+          <Column className="text-left" isRowHeader>
             Name
           </Column>
           {championship.extraPointsPublished ? (
             <>
-              <Column className="px-2 md:px-6">
+              <Column>
                 <span className="hidden sm:inline">Zusatzpunkte</span>
                 <span className="sm:hidden">Zusatzpkt</span>
               </Column>
-              <Column className="px-2 md:px-6">
+              <Column>
                 <span className="hidden sm:inline">Gesamtpunkte</span>
                 <span className="sm:hidden">Gesamt</span>
               </Column>
             </>
           ) : (
-            <Column className="px-2 md:px-6">Punkte</Column>
+            <Column>Punkte</Column>
           )}
 
           {!championship.completed && currentTips.length > 0 && (
-            <Column className="px-2 text-center">Tipps</Column>
+            <Column className="text-center md:px-2">Tipps</Column>
           )}
         </TableHeader>
-        <TableBody className="divide-y divide-default">
+        <TableBody>
           {ranks.map((player, ix) => {
             const rank =
               ix === 0
@@ -77,16 +79,14 @@ export default function RankingRoute() {
                   : '';
             return (
               <Row key={player.id}>
-                <Cell className="pr-4 text-right md:px-6">{rank}</Cell>
-                <Cell className="w-full px-2 py-2 md:px-6 sm:py-2.5">
-                  {player.user.name}
-                </Cell>
+                <Cell className="text-right">{rank}</Cell>
+                <Cell className="w-full">{player.user.name}</Cell>
                 {championship.extraPointsPublished && (
                   <Cell className="text-center">{player.extraPoints}</Cell>
                 )}
                 <Cell className="text-center">{player.totalPoints}</Cell>
                 {!championship.completed && currentTips.length > 0 && (
-                  <Cell className="px-4">
+                  <Cell>
                     <HoverBox>
                       <Button variant="trigger">
                         <Icon
