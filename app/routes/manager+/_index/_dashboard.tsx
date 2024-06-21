64684711eb179fc23d5be5fdcf6acd844d0d518d
Micha Buchholz <micha@haus23.net>
2024-06-21 22:20:27 +0200

    feat: Implement create championship. Resolves #150

--- app/routes/manager+/_index/_dashboard.tsx
+++ app/routes/manager+/_index/_dashboard.tsx
@@ -1,4 +1,7 @@
 import type { LoaderFunctionArgs } from '@remix-run/node';
+import clsx from 'clsx';
+import { Icon } from '#components/ui/icon/icon.js';
+import { Link } from '#components/ui/link/link.js';
 import type { IconName } from '#components/ui/types.js';
 import { requireAdmin } from '#utils/auth/auth.server';
 
@@ -18,13 +21,42 @@ const items: {
   {
     title: 'Neues Turnier',
     description: 'Starte eine neue Liga-Halbserie oder ein Turnier',
-    icon: 'folder',
+    icon: 'folder-plus',
     background: 'bg-pink-500',
-    route: './neues-turnier',
+    route: 'manager/neues-turnier',
     visible: () => true,
   },
 ];
 
 export default function DashboardRoute() {
-  return <div />;
+  return (
+    <ul className="mt-2 grid grid-cols-1 gap-6 py-6 sm:grid-cols-2">
+      {items
+        .filter((item) => item.visible())
+        .map((item) => (
+          <li key={item.title} className="sm:only:col-span-2 sm:only:mx-auto">
+            <Link
+              className="flex gap-x-3 p-2 hover:bg-content-hover"
+              href={item.route}
+            >
+              <div
+                className={clsx(
+                  'flex size-16 items-center justify-center rounded-lg',
+                  item.background,
+                )}
+              >
+                <Icon name={item.icon} />
+              </div>
+              <div className="flex flex-col">
+                <div>
+                  <span className="font-semibold">{item.title}</span>
+                  <span aria-hidden="true"> &rarr;</span>
+                </div>
+                <span className="text-app-subtle">{item.description}</span>
+              </div>
+            </Link>
+          </li>
+        ))}
+    </ul>
+  );
 }
--- app/routes/manager+/_masterdata/neues-turnier/_create-championship.tsx
+++ app/routes/manager+/_masterdata/neues-turnier/_create-championship.tsx
@@ -1,18 +1,25 @@
-import { useForm } from '@conform-to/react';
+import { useForm, useInputControl } from '@conform-to/react';
 import { parseWithZod } from '@conform-to/zod';
 import { type ActionFunctionArgs, json } from '@remix-run/node';
-import { useActionData, useSubmit } from '@remix-run/react';
+import { useActionData, useLoaderData, useSubmit } from '@remix-run/react';
 import type { FormEvent } from 'react';
 import { Form } from 'react-aria-components';
-import { z } from 'zod';
 import UI from '#components/ui';
-import { jsonWithToast } from '#utils/toast/toast.server.js';
+import { apiCommandBus } from '#utils/api/command-bus.server.js';
+import { createChampionshipSchema as schema } from '#utils/api/commands.js';
+import { db } from '#utils/db.server';
+import { redirectWithToast } from '#utils/toast/toast.server';
 
-const schema = z.object({
-  name: z.string(),
-  slug: z.string().regex(/^[a-z]{2}\d{4}$/),
-  nr: z.number().int(),
-});
+export const handle = { pageTitle: 'Neues Turnier' };
+
+export async function loader() {
+  const lastChampionship = await db.championship.findFirst({
+    orderBy: { nr: 'desc' },
+  });
+  const rulesets = await db.ruleset.findMany();
+
+  return json({ nextNr: (lastChampionship?.nr || 0) + 1, rulesets });
+}
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
@@ -22,20 +29,29 @@ export async function action({ request }: ActionFunctionArgs) {
     return json(submission.reply());
   }
 
-  if (submission.value.slug.startsWith('ss')) {
+  // Validate unique slug
+  const championship = await db.championship.findFirst({
+    where: { slug: submission.value.slug },
+  });
+  if (championship) {
     return json(
       submission.reply({ fieldErrors: { slug: ['Kennung schon vergeben'] } }),
     );
   }
 
-  console.log(`Creating ${JSON.stringify(submission.payload)}`);
-  return jsonWithToast(submission.reply, {
+  apiCommandBus.emit({
+    name: 'CREATE_CHAMPIONSHIP',
+    payload: submission.value,
+  });
+
+  return redirectWithToast(`../${submission.value.slug}`, {
     type: 'success',
     text: 'Turnier angelegt',
   });
 }
 
 export default function CreateChampionshipRoute() {
+  const { nextNr, rulesets } = useLoaderData<typeof loader>();
   const lastResult = useActionData<typeof action>();
   const submit = useSubmit();
 
@@ -46,7 +62,10 @@ export default function CreateChampionshipRoute() {
     },
     shouldValidate: 'onSubmit',
     shouldRevalidate: 'onBlur',
+    defaultValue: { nr: nextNr },
   });
+
+  const slugField = useInputControl(fields.slug);
 
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -59,9 +78,22 @@ export default function CreateChampionshipRoute() {
       Object.entries(lastResult.error).filter(([_, errors]) => errors !== null),
     ) as Record<string, string[]>);
 
+  function handleSlug() {
+    if (!fields.slug.dirty) {
+      const name = fields.name.value || '';
+      const stdPattern = /^([HRWE]).*(\d{2})\/?(\d{2})$/;
+      const match = name.match(stdPattern);
+      if (match?.[1] && match[2] && match[3]) {
+        const secondLetter = match[0].match(/[HR]/) ? 'r' : 'm';
+        const slug = `${match[1].toLowerCase() + secondLetter}${match[2] + match[3]}`;
+        slugField.change(slug);
+      }
+    }
+  }
+
   return (
     <UI.Card>
-      <UI.CardHeader id="tableLabel">Neues Turnier</UI.CardHeader>
+      <UI.CardHeader id="tableLabel">Turnier</UI.CardHeader>
       <UI.Divider />
       <UI.CardContent className="px-0 sm:px-4">
         <div>
@@ -72,30 +104,44 @@ export default function CreateChampionshipRoute() {
             id={form.id}
             onSubmit={handleSubmit}
           >
-            <UI.TextField>
+            <UI.TextField isRequired name={fields.name.name}>
               <UI.Label>Bezeichnung</UI.Label>
-              <UI.Input
-                required
-                type="text"
-                name={fields.name.name}
-                placeholder="Hinrunde 23/24 oder EM 2024"
-              />
+              <UI.Input type="text" placeholder="Hinrunde 23/24 oder EM 2024" />
               <UI.FieldError />
             </UI.TextField>
-            <UI.TextField isRequired pattern="[a-z]{2}\d{4}">
+            <UI.TextField
+              isRequired
+              name={fields.slug.name}
+              pattern="[a-z]{2}\d{4}"
+            >
               <UI.Label>Kennung</UI.Label>
-              <UI.Input type="text" name={fields.slug.name} />
+              <UI.Input type="text" onFocus={handleSlug} />
               <UI.Description hideOnError={false}>
                 Abkürzung in der Form hr2324 (Zwei Kleinbuchstaben und vier
                 Ziffern)
               </UI.Description>
               <UI.FieldError />
             </UI.TextField>
-            <UI.TextField isRequired>
+            <UI.TextField
+              isRequired
+              name={fields.nr.name}
+              defaultValue={fields.nr.initialValue}
+            >
               <UI.Label>Nummer</UI.Label>
-              <UI.Input type="number" name={fields.nr.name} />
+              <UI.Input type="number" />
               <UI.FieldError />
             </UI.TextField>
+            <UI.Select
+              label="Regelwerk"
+              isRequired
+              items={rulesets}
+              name={fields.rulesetId.name}
+              placeholder="Regelwerk auswählen"
+            >
+              {(item) => (
+                <UI.SelectItem id={item.id}>{item.name}</UI.SelectItem>
+              )}
+            </UI.Select>
             <div>
               <UI.Button variant="solid" color="accent" type="submit">
                 Anlegen
