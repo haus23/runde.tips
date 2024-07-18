4e0f1c197d04e842f4d417660a1999533bb1132e
Micha Buchholz <micha@haus23.net>
2024-07-18 15:36:54 +0200

    feat: Add individual nextui packages for card and divider.

--- apps/www/app/routes/_index.tsx
+++ apps/www/app/routes/_index.tsx
@@ -1,9 +1,27 @@
 import type { MetaFunction } from '@remix-run/node';
+import { Card, CardBody, CardHeader, Divider } from 'ui';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'runde.tips' }];
 };
 
 export default function Index() {
-  return <h1 className="p-4 font-semibold text-3xl">runde.tips</h1>;
+  return (
+    <>
+      <h1 className="p-4 font-semibold text-3xl">runde.tips</h1>
+      <Card className="mx-auto mt-4 max-w-4xl py-2">
+        <CardHeader className="px-8 text-2xl">Marie 23 Tipprunde</CardHeader>
+        <Divider className="my-2" />
+        <CardBody className="flex flex-col gap-y-4 px-8 text-lg">
+          <p>Willkommen bei unserer kleinen Fussball-Tipprunde!</p>
+          <p>
+            Leider gibt es noch keine Turniere und nichts zu tippen. Wir warten
+            einfach auf die erste Runde, die von der Spielleitung freigeschaltet
+            wird. Bis dahin empfehle ich ein kühles Blondes am Lieblingstresen
+            deiner Stadt.
+          </p>
+        </CardBody>
+      </Card>
+    </>
+  );
 }
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,2 @@
+export * from '@nextui-org/card';
+export * from '@nextui-org/divider';
