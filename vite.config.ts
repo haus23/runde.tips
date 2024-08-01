16f6b082903d9778888b3cec9cae3d681a51f6e0
Micha Buchholz <micha@haus23.net>
2024-08-01 21:36:42 +0200

    fix: Opt-In for single-fetch. Resolves #121

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig({
     remix({
       ignoredRouteFiles: ['**/*'],
       routes: async (defineRoutes) => flatRoutes('routes', defineRoutes),
+      future: {
+        unstable_singleFetch: true,
+      },
     }),
     tsconfigPaths(),
   ],
