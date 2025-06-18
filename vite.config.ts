4e2899a03a92762625c01b7b8cbe97ab96a2d0f8
Micha Buchholz <micha@haus23.net>
2025-06-18 16:59:50 +0200

    refactor: Move generated code to dedicated folder.

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,13 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [tsConfigPaths(), tanstackStart({ target: "netlify" })],
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart({
+      target: "netlify",
+      tsr: {
+        generatedRouteTree: "generated/routeTree.gen.ts",
+      },
+    }),
+  ],
 });
