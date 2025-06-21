593c22d580518d1241d1dfcf39e0b57db6904bb3
Micha Buchholz <micha@haus23.net>
2025-06-21 23:53:09 +0200

    chore: Add netlify plugin to build toolchain.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 
 import { reactRouter } from "@react-router/dev/vite";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [reactRouter(), netlifyPlugin()],
 });
