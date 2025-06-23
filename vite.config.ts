7c765ff90c22b6eef4a12a3a0f41e7ce03dc25a0
Micha Buchholz <micha@haus23.net>
2025-06-23 22:22:57 +0200

    chore(dx): Add path alias.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import { reactRouter } from "@react-router/dev/vite";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import tailwindCss from "@tailwindcss/vite";
 import devtoolsJson from "vite-plugin-devtools-json";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [devtoolsJson(), tailwindCss(), reactRouter(), netlifyPlugin()],
+  plugins: [
+    devtoolsJson(),
+    tsconfigPaths(),
+    tailwindCss(),
+    reactRouter(),
+    netlifyPlugin(),
+  ],
 });
