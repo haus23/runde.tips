984e97adbe94b73c6f5cb60f6d93663cc5c0380f
Micha Buchholz <micha@haus23.net>
2025-06-24 23:32:52 +0200

    feat: Implement initial take on route guards. See #2.

---
+++ app/utils/auth.server.ts
@@ -0,0 +1,34 @@
+import { redirect } from 'react-router';
+import type { Role, User } from '~/prisma';
+
+/**
+ * Loads user from db - may be identified by the cookie session
+ *
+ * @param request Request object
+ * @returns User or null
+ */
+async function getOptionalUser(_request: Request): Promise<User | null> {
+  return null;
+}
+
+/**
+ * Ensures no logged-in user
+ *
+ * @param request Request object
+ */
+export async function requireAnonymous(request: Request) {
+  const user = await getOptionalUser(request);
+  if (user) throw redirect('/');
+}
+
+/**
+ * Ensures logged-in user is manager
+ *
+ * @param request Request object
+ */
+export async function requireRole(request: Request, role: Role) {
+  const user = await getOptionalUser(request);
+  if (!user || user.role === role) {
+    throw redirect('/login');
+  }
+}
