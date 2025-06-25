7d9980b70acf44e2a85e3a67096451045effb5e9
Micha Buchholz <micha@haus23.net>
2025-06-25 11:04:09 +0200

    feat: Add minimal input wrapper. Resolves #16

---
+++ app/components/ui/input.tsx
@@ -0,0 +1,13 @@
+import type { InputProps as _InputProps } from 'react-aria-components';
+import { Input as _Input } from 'react-aria-components';
+import { cva } from '~/utils/cva';
+
+const input = cva({
+  base: ['flex w-full min-w-0 border px-3 py-1'],
+});
+
+interface InputProps extends _InputProps {}
+
+export function Input({ className, ...props }: InputProps) {
+  return <_Input className={input({ className })} {...props} />;
+}
