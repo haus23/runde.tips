d3531a5fae30227676111e18c43bbdfaa3e0d905
Micha Buchholz <micha@haus23.net>
2025-06-25 21:30:00 +0200

    refactor: Add some form styling. Could keep client code smaller.

--- app/components/ui/form/form.tsx
+++ app/components/ui/form/form.tsx
@@ -1,8 +1,13 @@
 import type { FormProps as _FormProps } from 'react-aria-components';
 import { Form as _Form } from 'react-aria-components';
+import { cva } from '~/utils/cva';
+
+const form = cva({
+  base: ['flex flex-col gap-4'],
+});
 
 interface FormProps extends _FormProps {}
 
-export function Form({ ...props }: FormProps) {
-  return <_Form {...props} />;
+export function Form({ className, ...props }: FormProps) {
+  return <_Form className={form({ className })} {...props} />;
 }
