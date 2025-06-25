68835df082f01b7882e1e7ebc610a76b27e17f43
Micha Buchholz <micha@haus23.net>
2025-06-25 21:30:34 +0200

    feat: Add minimal button wrapper. Resolves #18

---
+++ app/components/ui/button.tsx
@@ -0,0 +1,30 @@
+import type { ButtonProps as _ButtonProps } from 'react-aria-components';
+import { Button as _Button } from 'react-aria-components';
+import type { VariantProps } from '~/utils/cva';
+import { cva } from '~/utils/cva';
+
+const button = cva({
+  base: '',
+  variants: {
+    variant: {
+      unset: null,
+      primary: [],
+      secondary: [],
+    },
+  },
+  compoundVariants: [
+    {
+      variant: ['primary', 'secondary'],
+      className: 'border px-4 py-2 transition-transform active:scale-95',
+    },
+  ],
+  defaultVariants: {
+    variant: 'unset',
+  },
+});
+
+interface ButtonProps extends _ButtonProps, VariantProps<typeof button> {}
+
+export function Button({ className, variant, ...props }: ButtonProps) {
+  return <_Button className={button({ className, variant })} {...props} />;
+}
