ca899352388e2d2ca1d0dd86440da04e84ed3b47
Micha Buchholz <micha@haus23.net>
2025-06-25 22:45:17 +0200

    feat: Add verify code route. Resolves #4

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,6 @@ import { index, prefix, route } from '@react-router/dev/routes';
 export default [
   index('routes/foh/_index.tsx'),
   route('/login', 'routes/(auth)/login.tsx'),
+  route('/verify', 'routes/(auth)/verify.tsx'),
   ...prefix('hinterhof', [index('routes/hinterhof/_index.tsx')]),
 ] satisfies RouteConfig;
---
+++ app/routes/(auth)/verify.tsx
@@ -0,0 +1,8 @@
+export default function LoginRoute() {
+  return (
+    <div>
+      <title>Kontrolle - runde.tips</title>
+      <h1 className="font-medium text-2xl">Kontrolle</h1>
+    </div>
+  );
+}
