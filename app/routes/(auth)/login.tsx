101c55e48e2fd867d71a13c656ace0dbf66ec207
Micha Buchholz <micha@haus23.net>
2025-06-25 21:59:12 +0200

    feat: Add first take on the login form. Validates email. Resolves #24

--- app/routes/(auth)/login.tsx
+++ app/routes/(auth)/login.tsx
@@ -1,15 +1,50 @@
-import { requireAnonymous } from '~/utils/auth.server';
+import { useSubmit } from 'react-router';
+import { Button } from '~/components/ui/button';
+import { Form } from '~/components/ui/form/form';
+import { TextField } from '~/components/ui/form/text-field';
+import { prepareOnboarding, requireAnonymous } from '~/utils/auth.server';
 import type { Route } from './+types/login';
 
 export async function loader({ request }: Route.LoaderArgs) {
   await requireAnonymous(request);
 }
 
-export default function LoginRoute() {
+export async function action({ request }: Route.ActionArgs) {
+  await requireAnonymous(request);
+  return await prepareOnboarding(request);
+}
+
+export default function LoginRoute({ actionData }: Route.ComponentProps) {
+  const submit = useSubmit();
+
   return (
     <div>
       <title>Anmeldung - runde.tips</title>
       <h1 className="font-medium text-2xl">Anmeldung</h1>
+      <div className="mt-8 flex flex-col">
+        <Form
+          method="post"
+          onSubmit={(e) => {
+            e.preventDefault();
+            submit(e.currentTarget);
+          }}
+          validationErrors={actionData?.errors}
+        >
+          <TextField
+            label="Email"
+            name="email"
+            autoComplete="email"
+            type="email"
+            isRequired
+            description="Deine Adresse aus der Tipprunde"
+          />
+          <div>
+            <Button type="submit" variant="primary">
+              Code anfordern
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
