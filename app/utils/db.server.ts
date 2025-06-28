b3482a6c734eee1d19453ba01aa7b1c66160a4cc
Micha Buchholz <micha@haus23.net>
2025-06-28 14:57:18 +0200

    refactor: Include schema in drizzle instance to enable queries.

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
+import * as schema from '~/db/schema';
 import { env } from './env.server';
 
 const client = postgres(env.DATABASE_URL, { prepare: false });
-export const db = drizzle({ client, casing: 'snake_case' });
+export const db = drizzle({ client, schema, casing: 'snake_case' });
