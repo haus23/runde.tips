1384fa7a813132cde74e8a8734caded980bd9b68
Micha Buchholz <micha@haus23.net>
2025-07-01 11:50:23 +0200

    feat: Add players route to better test logout.

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,7 @@ import { index, prefix, route } from '@react-router/dev/routes';
 
 export default [
   index('routes/foh/_index.tsx'),
+  route('/spieler', 'routes/foh/players.tsx'),
   route('/login', 'routes/(auth)/login.tsx'),
   route('/verify', 'routes/(auth)/verify.tsx'),
   ...prefix('hinterhof', [index('routes/hinterhof/_index.tsx')]),
---
+++ app/routes/foh/players.tsx
@@ -0,0 +1,8 @@
+export default function PlayersRoute() {
+  return (
+    <div>
+      <title>runde.tips</title>
+      <h1 className="font-medium text-2xl">Spieler</h1>
+    </div>
+  );
+}
