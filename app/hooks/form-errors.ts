1b9965dd0f00f1e880822e3060ba4cde5180f305
Micha Buchholz <micha@haus23.net>
2025-07-03 11:57:04 +0200

    fix: Do not merge the errors. Resolves #59

--- app/hooks/form-errors.ts
+++ app/hooks/form-errors.ts
@@ -1,32 +1,23 @@
 import type { AnyFormApi } from '@tanstack/react-form';
 
 import { useStore } from '@tanstack/react-form';
-import { useMemo } from 'react';
 
 export function useFormErrors<TForm extends AnyFormApi>(
   form: TForm,
-  mergedErrors?: Record<string, string>,
+  serverErrors?: Record<string, string>,
 ) {
   const fieldMeta = useStore(form.store, (state) => state.fieldMeta);
-  return useMemo(() => {
-    const formErrors = Object.entries(fieldMeta).reduce(
-      (acc, [key, value]) => {
-        if (value.errors.length > 0) {
-          acc[key] = value.errors;
-        }
-        return acc;
-      },
-      {} as Record<string, string[]>,
-    );
-    return mergedErrors
-      ? Object.entries(mergedErrors).reduce((acc, [key, error]) => {
-          if (acc[key]) {
-            acc[key].push(error);
-          } else {
-            acc[key] = [error];
-          }
-          return acc;
-        }, formErrors)
-      : formErrors;
-  }, [fieldMeta, mergedErrors]);
+
+  const formErrors = Object.entries(fieldMeta).reduce(
+    (acc, [key, value]) => {
+      if (value.errors.length > 0) {
+        acc[key] = value.errors;
+      }
+      return acc;
+    },
+    {} as Record<string, string[]>,
+  );
+
+  // Return formErros if any. Otherwise the untouched server errors.
+  return Object.keys(formErrors).length > 0 ? formErrors : serverErrors;
 }
